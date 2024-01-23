8b158e00d23775e6a71a28ee044295cd2f2888a6
deniservat <deniservat@gmail.com>
2024-01-23 19:13:20 -0300

    manana sera otro dia

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//NEWSLETTER
+
+$.ajax({
+    url: "https://formsubmit.co/ajax/deniservat@gmail.com",
+    method: "POST",
+    data: {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    },
+    dataType: "json"
+});
