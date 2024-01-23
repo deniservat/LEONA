73d9a2e358aa70553c432d6031fc0f6b37e10492
deniservat <deniservat@gmail.com>
2024-01-23 11:52:34 -0300

    index y productos pages terminadas

--- js/main.js
+++ js/main.js
@@ -47,3 +47,17 @@ optionSelected.addEventListener("click", (e) => {
             optionSelected.innerHTML = options[0];
            }},{once:true});
  */
+
+
+
+//NEWSLETTER
+
+$.ajax({
+    url: "https://formsubmit.co/ajax/deniservat@gmail.com",
+    method: "POST",
+    data: {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    },
+    dataType: "json"
+});
